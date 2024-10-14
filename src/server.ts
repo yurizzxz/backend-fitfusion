f2de2b5476de711801d3c6f1ef1433e6d31ef40a
yurizzxz <yuri.alves.silva@gmail.com>
2024-10-14 14:03:19 -0300

    teste2

--- src/server.ts
+++ src/server.ts
@@ -6,20 +6,33 @@ import { routes } from './routes';
 const app = Fastify({ logger: true });
 dotenv.config();
 
+// Handler para capturar e retornar erros
 app.setErrorHandler((error, request, reply) => {
   reply.code(400).send({ message: error.message });
 });
 
 const start = async () => {
+  // Configurando o CORS para aceitar localhost e a API no Vercel
   app.register(cors, {
-    origin: true, // Permite todas as origens
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'], // Adiciona cabeçalhos permitidos
-    credentials: true,
+    origin: (origin, cb) => {
+      // Lista de origens permitidas
+      const allowedOrigins = ["http://localhost:3000", "https://backend-fitfusion-ymwv.vercel.app"];
+      // Verifica se a origem está permitida
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        cb(null, true);  // Permite a requisição
+      } else {
+        cb(new Error("Not allowed by CORS")); // Bloqueia origens não permitidas
+      }
+    },
+    methods: ['GET', 'POST', 'OPTIONS'],  // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    credentials: true, // Permite cookies e autenticação via credenciais
   });
 
+  // Registrando as rotas
   app.register(routes);
 
+  // Iniciando o servidor
   try {
     await app.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
     console.log(`Servidor rodando em http://localhost:${process.env.PORT || 3000}`);
